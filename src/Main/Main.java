8c2ae7a38e3bd51a1c3a83b2e6f68c7593b159fa
Dilan <ramadhanrizqi21@gmail.com>
2020-10-20 22:43:08 +0700

    first

---
+++ src/Main/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Muhammad Alvin Rizqi Ramadhan
+ * @author NIM 10119913
+ */
+public class Main {
+    
+    //memberi nilai ke variable
+    
+    public static void main(String[] args) {
+        //Deklarasi variable
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        // TODO code application logic here
+        nilaiInt = 78;    
+        nilaiLogika =  false;
+        nilaiKarakter = 'D';
+        System.out.println();
+        System.out.println("Isi variable nilai = "+nilaiInt);
+        System.out.println("Isi variable PHI = "+PHI);
+        System.out.println("Isi variable Logika = "+nilaiLogika);
+        System.out.println("Isi variable Karakter = "+nilaiKarakter);
+
+    }
+}
